903eba51c1369a02e65614639bae45b977e708e0
Bruna Franciele <brunafranciele1902@gmail.com>
2020-12-18 15:38:00 -0300

    feat: redirect logic

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import tarotContext from '../context/tarotContext';
 
 function Login() {
   const [nameLogin, setNameLogin] = useState('');
   const [isDisabled, setIsDisabled] = useState(true);
+  const { setName } = useContext(tarotContext);
 
   useEffect(() => {
     const two = 2;
@@ -11,6 +14,14 @@ function Login() {
       setIsDisabled(false);
     }
   }, [nameLogin]);
+
+  const handlePath = () => {
+    window.location.replace('http://localhost:3000/main-page');
+  };
+
+  const handleRedirect = () => {
+    setName(nameLogin);
+  };
 
   return (
     <form>
@@ -23,15 +34,18 @@ function Login() {
             onChange={({ target }) => setNameLogin(target.value)}
           />
         </label>
-        <button
-          type="button"
-          disabled={ isDisabled }
-        >
-          Start
-        </button>
+        <Link to="/main-page">
+          <button
+            type="button"
+            disabled={isDisabled}
+            onClick={handleRedirect}
+          >
+            Start
+          </button>
+        </Link>
       </fieldset>
     </form>
-    
+
   );
 }
 
